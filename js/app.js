5d47cb85e1b74b043e15e12e01d5bd14da33b1f7
kevin <kienvm.dev@gmail.com>
2019-06-17 08:20:06 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
-    $('a[href*=#]').bind('click', function(e) {
-        e.preventDefault(); // prevent hard jump, the default behavior
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
 
-        var target = $(this).attr("href"); // Set the target as variable
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-        // perform animated scrolling by getting top-position of target-element and set it as scroll target
-        $('html, body').stop().animate({
-            scrollTop: $(target).offset().top
-        }, 600, function() {
-            location.hash = target; //attach the hash (#jumptarget) to the pageurl
-        });
+            // Store hash
+            var hash = this.hash;
 
-        return false;
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
     });
 });
 

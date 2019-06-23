6cc0095f8d7d3d38eae0e73391b89eee384d6704
kevin <kienvm.dev@gmail.com>
2019-06-23 23:48:22 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,43 +1,59 @@
-// $(document).ready(function() {
-//     // Add smooth scrolling to all links
-//     $("a").on('click', function(event) {
+$(document).ready(function() {
 
-//         // Make sure this.hash has a value before overriding default behavior
-//         if (this.hash !== "") {
-//             // Prevent default anchor click behavior
-//             event.preventDefault();
+    var heightWindow = $(window).innerHeight();
+    var heightHomeBanner = $("#banner").innerHeight();
+    var heightHomeAction = $("#action").innerHeight();
+    var scrollToHomeDataTop = heightHomeBanner + heightHomeAction - heightWindow * 0.7;
 
-//             // Store hash
-//             var hash = this.hash;
+    $(window).scroll(function() {
+        var scroll = $(this).scrollTop();
+        if (scroll >= scrollToHomeDataTop) {
+            $("#solieu .col1").addClass("active");
+        } else {
+            $("#solieu .col1").removeClass("active");
+        }
+    });
 
-//             // Using jQuery's animate() method to add smooth page scroll
-//             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//             $('html, body').animate({
-//                 scrollTop: $(hash).offset().top
-//             }, 800, function() {
+    $('nav a').on('click', function() {
 
-//                 // Add hash (#) to URL when done scrolling (default click behavior)
-//                 window.location.hash = hash;
-//             });
-//         } // End if
-//     });
-// });
+        var scrollAnchor = $(this).attr('data-scroll'),
+            scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 28;
 
-// $(window).scroll(function() {
-//     var scrollDistance = $(window).scrollTop();
+        $('body,html').animate({
+            scrollTop: scrollPoint
+        }, 500);
 
-//     // Show/hide menu on scroll
-//     //if (scrollDistance >= 850) {
-//     //		$('nav').fadeIn("fast");
-//     //} else {
-//     //		$('nav').fadeOut("fast");
-//     //}
+        return false;
 
-//     // Assign active class to nav links while scolling
-//     $('section').each(function(i) {
-//         if ($(this).position().top <= scrollDistance) {
-//             $('.navbar-nav li.active').removeClass('active');
-//             $('.navbar-nav li').eq(i).addClass('active');
-//         }
-//     });
-// }).scroll();
+    })
+
+
+    $(window).scroll(function() {
+        var windscroll = $(window).scrollTop();
+        if (windscroll >= 100) {
+            // $('nav').addClass('fixed');
+            $('#main section').each(function(i) {
+                if ($(this).position().top <= windscroll - 20) {
+                    $('nav li.active').removeClass('active');
+                    $('nav li').eq(i).addClass('active');
+                }
+            });
+
+        } else {
+
+            // $('nav').removeClass('fixed');
+            $('nav li.active').removeClass('active');
+            $('nav li:first').addClass('active');
+        }
+
+    }).scroll();
+
+    $("#tab a").click(function() {
+
+    });
+
+    $("a.btn-light").click(function() {
+        $('#tab .first').removeClass('active');
+        // $('#tab .first').addClass('active'); 
+    });
+});
